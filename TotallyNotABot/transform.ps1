e59199687b0f5831d492afe584754bccd55ddf4d
maxderegt <maxderegt@users.noreply.github.com>
2018-09-06 17:50:33 +0200

    fixes for windows server 2016

--- TotallyNotABot/transform.ps1
+++ TotallyNotABot/transform.ps1
@@ -3,3 +3,8 @@ dotnet clean -c Release
 dotnet restore
 dotnet build -c Release -f netcoreapp2.0
 dotnet publish -c Release -f netcoreapp2.0 -r ubuntu.16.10-x64
+
+dotnet clean -c Release
+dotnet restore
+dotnet build -c Release -f netcoreapp2.0
+dotnet publish -c Release -r win10-x64
